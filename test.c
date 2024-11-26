6f3abbf430bbed9c4d7559e1c8ca76cd21a0a1f1
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2024-11-25 19:05:13 -0800

    Made the small malloc test

--- test.c
+++ test.c
@@ -1,32 +1,23 @@
-// Add header later
+// Kshitij Pingle
+// kpingle@csu.fullerton.edu
+// Date: November 25, 2024
+// Description: Program to test my implementation of malloc and free
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <time.h>
 
-#define NUM_OF_TESTS 10
+#define NUM_OF_TESTS 5
 
 int rand_between(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
+bool small_test() {
 
-int main(int argc, char *argv[]) {
-    srand(time(NULL));
-    int num_of_tests = NUM_OF_TESTS;
-
-    // Accept arguments
-    if (argc > 1) {
-        if ((strcmp(argv[1], "-t") == 0) && (strcmp(argv[2], "num") == 0)) {
-            num_of_tests = atoi(argv[3]);
-        }
-        else {
-            printf("Error: Invalid arguments\n");
-            printf("Usage: %s -t num <num_of_tests>\n", argv[0]);
-            return 1;
-        }
-    }
+    int num_of_tests = 5;
 
     // 64 chars long currently
     char *test_string = "This is a test string for Kshitij to test his mallocs and frees.";
@@ -43,7 +34,7 @@ int main(int argc, char *argv[]) {
         ptrs[i] = malloc(rand_size);
         if (ptrs[i] == NULL) {
             fprintf(stderr, "Failed to allocate memory for ptrs[%d]\n", i);
-            return 1;
+            return false;
         }
 
         int len_to_copy = (rand_size > strlen(test_string)) ? strlen(test_string) : rand_size;  // Give the min of the two
@@ -62,6 +53,49 @@ int main(int argc, char *argv[]) {
         free(ptrs[i]);
         fprintf(stderr, "Freed ptrs[%d]\n", i);
     }
+
+    // If we reached here, all tests passed
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    srand(time(NULL));
+
+    bool result = false;
+
+    if (argc > 1) {
+        if ((strcmp(argv[1], "-t") == 0) && (strcmp(argv[2], "num") == 0)) {
+            // Execute only one random test
+            int test_num = atoi(argv[3]);
+
+            switch (test_num) {
+                case 1:
+                    printf("Executing small_test()\n");
+                    result = small_test();
+                    printf("RESULT: %s\n", result ? "PASSED" : "FAILED");
+                    break;
+                default:
+                    printf("Executing small_test()\n");
+                    result = small_test();
+                    printf("RESULT: %s\n", result ? "PASSED" : "FAILED");
+                    break;
+            }
+
+            return 0;
+        }
+        else {
+            printf("Error: Invalid arguments\n");
+            printf("Usage: %s -t num\n", argv[0]);
+            return 1;
+        }
+    }
+
+    // Otherwise execute all tests
+
+    printf("Executing small_test()\n");
+    result = small_test();
+    printf("RESULT: %s\n", result ? "PASSED" : "FAILED");
+
 
     return 0;
 }
